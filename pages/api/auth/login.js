756369bbcec31d3d7f1b33224530af3ce68e5811
ThePurple74 <108777695+ThePurple74@users.noreply.github.com>
2023-08-06 01:38:21 +0900

    no secure

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -30,10 +30,7 @@ export default async function handler(req, res) {
   const jwt = sign({ id: user.id }, process.env.JWT_KEY);
 
   console.log(jwt);
-  res.setHeader(
-    "Set-Cookie",
-    serialize("authorization", jwt, { path: "/", secure: true })
-  );
+  res.setHeader("Set-Cookie", serialize("authorization", jwt, { path: "/" }));
 
   return res.status(200).send(user);
 }
